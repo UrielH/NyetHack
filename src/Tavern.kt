1ff59e66dc3bd341a5f6a1acdcd5b76115b65472
Uriel HI <uriheri.95@gmail.com>
2020-01-28 00:13:41 -0600

    Using let +  safe call operator.

--- src/Tavern.kt
+++ src/Tavern.kt
@@ -1,7 +1,18 @@
 fun main () {
     //Using safe call operator
-    var beverage = readLine()?.capitalize()
+ /*   var beverage = readLine()?.capitalize()
     beverage = null
-    print(beverage)
+     print(beverage)*/
 
+    // Using let operator + safe call operator
+    var beverage = readLine()?.let {
+        if(it.isNotBlank()){
+            it.capitalize()
+        } else {
+            "Buttered Ale"
+        }
+    }
+   // beverage = null
+
+    print(beverage)
 }
