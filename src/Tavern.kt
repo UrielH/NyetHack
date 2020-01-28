4760a3029fb3419b7b6f1d353457cbe7ac41fb6c
Uriel HI <uriheri.95@gmail.com>
2020-01-28 00:19:41 -0600

    Using non-null assertion operator

--- src/Tavern.kt
+++ src/Tavern.kt
@@ -5,14 +5,17 @@ fun main () {
      print(beverage)*/
 
     // Using let operator + safe call operator
-    var beverage = readLine()?.let {
+  /*  var beverage = readLine()?.let {
         if(it.isNotBlank()){
             it.capitalize()
         } else {
             "Buttered Ale"
         }
-    }
+    }*/
    // beverage = null
+
+    // Using non-null assertion operator
+    var beverage = readLine()!!.capitalize()
 
     print(beverage)
 }
